552525acea308b87c32a795c728f7fb8c51a56fb
orange22 <dmitriy.bozhok@gmail.com>
2014-08-21 18:26:07 +0300

    123

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
